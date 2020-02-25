edaa6ed7bb9ee60c3921044153dbd668ca314973
Shubham Singh <singh.shubham0812@gmail.com>
2020-02-25 20:32:02 +0530

    Modifications to Service

--- src/app/services/data-fetch.service.ts
+++ src/app/services/data-fetch.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IRows, ITabs } from '../model/table-model';
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ export class DataFetchService {
 
   private tableData: IRows[] = [{
     date: '',
+    index: 0,
     listName: 'Competitive Intelligence',
     noOfEntities: 2,
     actions: ['mail', 'share', 'edit', 'delete'],
@@ -16,49 +18,55 @@ export class DataFetchService {
     date: '',
     listName: 'My Vendors',
     noOfEntities: 202,
+    index: 1,
     actions: ['mail', 'share', 'edit'],
     details: []
   }, {
     date: 'Jun 23',
+    index: 2,
     listName: 'My Customers',
     noOfEntities: 24,
     actions: ['mail', 'share'],
-    details: ['Bundi Technologies Private Limited']
+    details: ['Bundi Technologies Private Limited', 'Hector Beverages Private Limited', 'Myntra Technologies']
   }, {
     date: 'Aug 13',
+    index: 3,
     listName: 'Test 30 Results',
     noOfEntities: 15,
     actions: ['mail', 'share', 'delete'],
-    details: ['Bundi Technologies Private Limited']
+    details: ['Bundi Technologies Private Limited', 'Google', 'Facebook', 'Hector Beverages Private Limited']
   }, {
     date: '',
+    index: 4,
     listName: 'Mumbai',
     noOfEntities: 95,
     actions: ['mail', 'share', 'edit', 'delete'],
     details: ['Bundi Technologies Private Limited']
   }, {
     date: 'Feb 28 ',
+    index: 5,
     listName: 'Bengaluru',
     noOfEntities: 89,
     actions: [],
     details: ['Bundi Technologies Private Limited']
   }, {
     date: 'Aug 5',
+    index: 6,
     listName: 'Custom Company',
     noOfEntities: 59,
     actions: ['mail', 'edit', 'delete'],
     details: ['Bundi Technologies Private Limited']
   }, {
     date: 'Dec 13',
+    index: 7,
     listName: 'Two Companies',
     noOfEntities: 148,
     actions: ['mail'],
     details: ['Bundi Technologies Private Limited']
   },
-
-
-
   ];
+
+
 
 
   private tabData: ITabs[] = [{
@@ -78,6 +86,14 @@ export class DataFetchService {
     route: '/'
   }];
 
+
+  private searchQuery = new BehaviorSubject('');
+  currentSearch = this.searchQuery.asObservable();
+
+
+  private selectedRow = new BehaviorSubject(10);
+  currentRow = this.selectedRow.asObservable();
+
   constructor() { }
 
   getTabs() {
@@ -86,5 +102,13 @@ export class DataFetchService {
 
   getTableData() {
     return this.tableData;
+  }
+
+  changeSearchQuery(value: string) {
+    this.searchQuery.next(value);
+  }
+
+  changeRow(value: number) {
+    this.selectedRow.next(value);
   }
 }
