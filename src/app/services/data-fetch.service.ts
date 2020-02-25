c1235c184a53cabd71ccbb1c052191a73811d99e
Shubham Singh <singh.shubham0812@gmail.com>
2020-02-25 16:56:00 +0530

    Modifications to Service

--- src/app/services/data-fetch.service.ts
+++ src/app/services/data-fetch.service.ts
@@ -1,18 +1,64 @@
 import { Injectable } from '@angular/core';
-import { ITable, ITabs } from '../model/table-model';
+import { IRows, ITabs } from '../model/table-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataFetchService {
 
-  private tableData: ITable[] = [{
+  private tableData: IRows[] = [{
     date: '',
-    listName: '',
-    noOfEntries: 1,
+    listName: 'Competitive Intelligence',
+    noOfEntities: 2,
+    actions: ['mail', 'share', 'edit', 'delete'],
+    details: ['Bundi Technologies Private Limited', 'Hector Beverages Private Limited', 'Infosys Limited', 'Vodafone Idea Limited']
+  }, {
+    date: '',
+    listName: 'My Vendors',
+    noOfEntities: 202,
+    actions: ['mail', 'share', 'edit'],
+    details: []
+  }, {
+    date: 'Jun 23',
+    listName: 'My Customers',
+    noOfEntities: 24,
     actions: ['mail', 'share'],
     details: ['Bundi Technologies Private Limited']
-  }];
+  }, {
+    date: 'Aug 13',
+    listName: 'Test 30 Results',
+    noOfEntities: 15,
+    actions: ['mail', 'share', 'delete'],
+    details: ['Bundi Technologies Private Limited']
+  }, {
+    date: '',
+    listName: 'Mumbai',
+    noOfEntities: 95,
+    actions: ['mail', 'share', 'edit', 'delete'],
+    details: ['Bundi Technologies Private Limited']
+  }, {
+    date: 'Feb 28 ',
+    listName: 'Bengaluru',
+    noOfEntities: 89,
+    actions: [],
+    details: ['Bundi Technologies Private Limited']
+  }, {
+    date: 'Aug 5',
+    listName: 'Custom Company',
+    noOfEntities: 59,
+    actions: ['mail', 'edit', 'delete'],
+    details: ['Bundi Technologies Private Limited']
+  }, {
+    date: 'Dec 13',
+    listName: 'Two Companies',
+    noOfEntities: 148,
+    actions: ['mail'],
+    details: ['Bundi Technologies Private Limited']
+  },
+
+
+
+  ];
 
 
   private tabData: ITabs[] = [{
@@ -36,5 +82,9 @@ export class DataFetchService {
 
   getTabs() {
     return this.tabData;
+  }
+
+  getTableData() {
+    return this.tableData;
   }
 }
